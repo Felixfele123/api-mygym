570a35c772f0ae9beb66c12edacdc08654c0f2c9
Felix <felixfralla@hotmail.com>
2022-08-15 16:30:05 +0200

    .

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const auth = require('./routes/auth')
 const menu = require('./routes/menu');
 const login = require('./routes/login')
 const logout = require('./routes/logout')
+const bodyParser = require('body-parser')
+
 
 
 mongoose.connect('mongodb+srv://felixzandereriksson:Jesper.nu1@cluster0.9idaz.mongodb.net/vaxtorpspizzeria', { useUnifiedTopology: true, useNewUrlParser: true})
@@ -17,6 +19,8 @@ mongoose.connect('mongodb+srv://felixzandereriksson:Jesper.nu1@cluster0.9idaz.mo
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({origin: true, credentials: true}))
+app.use(bodyParser.urlencoded({ extended: true}));
+
 //nytt
 app.use('/menu', menu);
 app.use('/login', login)
