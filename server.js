ab986a0a7511449b4863bec0669ee60dafa615c5
Felix <felixfralla@hotmail.com>
2022-08-15 14:50:38 +0200

    .

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+//gammalt
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
 const cors = require('cors')
 require('dotenv').config();
-const schema = require('./routes/schema');
-const workout = require('./routes/workout');
+//nytt
+const cookieParser = require("cookie-parser");
+const auth = require('./routes/auth')
+const menu = require('./routes/menu');
+const login = require('./routes/login')
+const logout = require('./routes/logout')
+const bodyParser = require('body-parser')
+require('dotenv').config();
 
 mongoose.connect('mongodb+srv://felixzandereriksson:Jesper.nu1@cluster0.9idaz.mongodb.net/vaxtorpspizzeria', { useUnifiedTopology: true, useNewUrlParser: true})
 .then(() => console.log('connected to MongoDB..'))
 .catch(err => console.error('could not connect', err))
 
+//gammalt
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({origin: true, credentials: true}))
-app.use('/schema', schema);
-app.use('/workout', workout);
+
+//nytt
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(cors({origin: true, credentials: true}))
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/menu', menu);
+app.use('/login', login)
+app.use('/secret', auth)
+app.use('/logout', logout)
 
 const server = app.listen(process.env.PORT, () => console.log("port " + process.env.PORT));
