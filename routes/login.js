09f1f99d478bc6e0054f8d499230f10a48368ab6
Felix <felixfralla@hotmail.com>
2022-08-18 13:46:10 +0200

    .

--- routes/login.js
+++ routes/login.js
@@ -25,7 +25,7 @@ router.post("/", async (req,res) => {
 
                 const token = jwt.sign(userObject, process.env.FIRSTSECRET, {expiresIn:120000});
                 console.log(token)
-                let result = res.cookie('token',token,{httpOnly:true,sameSite:'none', secure: 'false'});
+                let result = res.cookie('token',token,{httpOnly:true,sameSite:'strict'});
                 res.send();
             }
             else{
